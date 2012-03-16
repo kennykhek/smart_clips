9ca8cfc55a25c74e5ada55e31e8533ed433cd5ac
hock <kohkh1987@gmail.com>
2012-03-16 20:34:20 +0800

    added a .cs file for each phase for code clarity

---
+++ CS4244/MobilePhone/PhasePhoneSpecs.cs
@@ -0,0 +1,159 @@
+﻿using Mommosoft.ExpertSystem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace MobilePhone
+{
+    public partial class MainForm : Form
+    {
+        List<String> listModel;
+        List<int> listPrice;
+        List<String> listBrand;
+        List<String> listColor;
+        List<float> listScreen;
+        List<int> listWeight;
+        List<int> listMem;
+        List<String> listOS;
+        List<String> listBluetooth;
+        List<String> listWifi;
+        List<String> listFM;
+        List<int> listZoom;
+        List<String> listFlash;
+        List<String> listVideoHD;
+        List<int> listPixel;
+
+        public void ProcessPhoneSpecs()
+        {
+        }
+
+        public void LoadPhaseDetailsDropdown()
+        {
+            listModel = new List<String>();
+            listPrice = new List<int>();
+            listBrand = new List<String>();
+            listColor = new List<String>();
+            listScreen = new List<float>();
+            listWeight = new List<int>();
+            listMem = new List<int>();
+            listOS = new List<String>();
+            listBluetooth = new List<String>();
+            listWifi = new List<String>();
+            listFM = new List<String>();
+            listZoom = new List<int>();
+            listFlash = new List<String>();
+            listVideoHD = new List<String>();
+            listPixel = new List<int>();
+
+            string evalStr = "(get-mobilephone-list)";
+            MultifieldValue mv = (MultifieldValue)environment.Eval(evalStr);
+            for (int i = 0; i < mv.Count; i++)
+            {
+                FactAddressValue fv = (FactAddressValue)mv[i];
+
+                String sModel = (String)(SymbolValue)fv.GetFactSlot("model");
+                listModel.Add(sModel);
+
+                int iPrice = (int)(IntegerValue)fv.GetFactSlot("price");
+                listPrice.Add(iPrice);
+
+                String sBrand = (String)(SymbolValue)fv.GetFactSlot("brand");
+                listBrand.Add(sBrand);
+
+                String sColor = (String)(SymbolValue)fv.GetFactSlot("color");
+                listColor.Add(sColor);
+
+                float fScreen = (float)(FloatValue)fv.GetFactSlot("screen");
+                listScreen.Add(fScreen);
+
+                int iWeight = (int)(IntegerValue)fv.GetFactSlot("weight");
+                listWeight.Add(iWeight);
+
+                int iMem = (int)(IntegerValue)fv.GetFactSlot("memory");
+                listMem.Add(iMem);
+
+                String sOS = (String)(SymbolValue)fv.GetFactSlot("os");
+                listOS.Add(sOS);
+
+                String sBluetooth = (String)(SymbolValue)fv.GetFactSlot("bluetooth");
+                listBluetooth.Add(sBluetooth);
+
+                String sWifi = (String)(SymbolValue)fv.GetFactSlot("wifi");
+                listWifi.Add(sWifi);
+
+                String sFM = (String)(SymbolValue)fv.GetFactSlot("fm");
+                listFM.Add(sFM);
+
+                int iCameraZoom = (int)(IntegerValue)fv.GetFactSlot("zoom");
+                listZoom.Add(iCameraZoom);
+
+                int iCameraPixel = (int)(IntegerValue)fv.GetFactSlot("pixel");
+                listZoom.Add(iCameraPixel);
+
+                String sFlash = (String)(SymbolValue)fv.GetFactSlot("flash");
+                listFlash.Add(sFlash);
+
+                String sVideoHD = (String)(SymbolValue)fv.GetFactSlot("videoHD");
+                listVideoHD.Add(sVideoHD);
+            }
+
+
+            listModel = listModel.Distinct().ToList();
+            listPrice = listPrice.Distinct().ToList();
+            listBrand = listBrand.Distinct().ToList();
+            listColor = listColor.Distinct().ToList();
+            listScreen = listScreen.Distinct().ToList();
+            listWeight = listWeight.Distinct().ToList();
+            listMem = listMem.Distinct().ToList();
+            listOS = listOS.Distinct().ToList();
+            listBluetooth = listBluetooth.Distinct().ToList();
+            listWifi = listWifi.Distinct().ToList();
+            listFM = listFM.Distinct().ToList();
+            listZoom = listZoom.Distinct().ToList();
+            listFlash = listFlash.Distinct().ToList();
+            listVideoHD = listVideoHD.Distinct().ToList();
+
+            //for (int i = 0; i < listModel.Count; i++)
+              
+  
+            //for (int i = 0; i < listPrice.Count; i++)
+
+            for (int i = 0; i < listColor.Count; i++)
+                cbColor.Items.Add(listColor.ElementAt(i));
+
+            for (int i = 0; i < listScreen.Count; i++)
+                cbScreen.Items.Add(listScreen.ElementAt(i));
+
+            for (int i = 0; i < listFM.Count; i++)
+                cbFM.Items.Add(listFM.ElementAt(i));
+
+            for (int i = 0; i < listVideoHD.Count; i++)
+                cbVideoHD.Items.Add(listVideoHD.ElementAt(i));
+
+            for (int i = 0; i < listFlash.Count; i++)
+                cbCamFlash.Items.Add(listFlash.ElementAt(i));
+
+            for (int i = 0; i < listMem.Count; i++)
+                cbMem.Items.Add(listMem.ElementAt(i));
+
+            for (int i = 0; i < listWeight.Count; i++)
+                cbWeight.Items.Add(listWeight.ElementAt(i));
+
+            for (int i = 0; i < listPixel.Count; i++)
+                cbCamPixel.Items.Add(listPixel.ElementAt(i));
+
+            for (int i = 0; i < listWifi.Count; i++)
+                cbWifi.Items.Add(listWifi.ElementAt(i));
+
+            for (int i = 0; i < listZoom.Count; i++)
+                cbZoom.Items.Add(listZoom.ElementAt(i));
+        }
+    }
+}
---
+++ CS4244/MobilePhone/PhasePreferences.cs
@@ -0,0 +1,108 @@
+﻿using Mommosoft.ExpertSystem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MobilePhone
+{
+    public partial class MainForm : Form
+    {
+        public void ProcessPhasePreferences()
+        {
+            /*
+              * For each of the question asked in phase 2, the for loop is the same, just note the box name is
+              * different
+              * @kwanghock
+           */
+            //Assert the weightage of the attributes related to the question here
+            //Question 1
+            // foreach (RadioButton control in grp_box_q1.Controls) //Throw error here as grp_box_q1 has label as well @kwanghock 11march2012
+            foreach (RadioButton control in rbBoxQns1.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you watch movies you downloaded on your phone?
+                    if (control.Name.Equals("q1_yes"))
+                    {
+                        environment.AssertString("(question (order watch_movie) (selection yes))");
+                    }
+                }
+            }
+            //Question 2
+            //foreach (RadioButton control in grp_box_q2.Controls)
+            foreach (RadioButton control in rbBoxQns2.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you listen to music on your phone?
+                    if (control.Name.Equals("q2_yes"))
+                    {
+                        environment.AssertString("(question (order listen_music) (selection yes))");
+                    }
+
+                }
+            }
+
+            //Question 3
+            // foreach (RadioButton control in grp_box_q3.Controls)
+            foreach (RadioButton control in rbBoxQns3.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you view pictures you downloaded on your phone?
+                    if (control.Name.Equals("q3_yes"))
+                    {
+                        environment.AssertString("(question (order view_picture) (selection yes))");
+                    }
+                }
+            }
+
+            //Question 4
+            // foreach (RadioButton control in grp_box_q4.Controls)
+            foreach (RadioButton control in rbBoxQns4.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you play online or Internet browsing on your phone?
+                    if (control.Name.Equals("q4_yes"))
+                    {
+                        environment.AssertString("(question (order game_internet) (selection yes))");
+                    }
+                }
+            }
+
+            //Question 5
+            // foreach (RadioButton control in grp_box_q5.Controls)
+            foreach (RadioButton control in rbBoxQns5.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you use phone as your personal and only camera? 
+                    if (control.Name.Equals("q5_yes"))
+                    {
+                        environment.AssertString("(question (order use_camera) (selection yes))");
+                    }
+                }
+            }
+
+            //Question 6
+            // foreach (RadioButton control in grp_box_q6.Controls)
+            foreach (RadioButton control in rbBoxQns6.Controls)
+            {
+                if (control.Checked)
+                {
+                    //Question: Do you often take night picture/video using your phone
+                    if (control.Name.Equals("q6_yes"))
+                    {
+                        environment.AssertString("(question (order use_camera_night) (selection yes))");
+                    }
+                }
+            }
+        }
+    }
+}
